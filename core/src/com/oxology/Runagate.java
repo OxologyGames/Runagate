45cdb4458972ce8a874bcbe3bd44c4010a4cee63
OxologyGames <oxology.games@gmail.com>
2023-08-27 16:07:58 +0200

    Updated Main class

--- core/src/com/oxology/Runagate.java
+++ core/src/com/oxology/Runagate.java
@@ -1,31 +1,17 @@
 package com.oxology;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
+import com.oxology.screen.menu.Main;
 
-public class Runagate extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class Runagate extends Game {
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		this.setScreen(new Main());
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		super.render();
 	}
 }
