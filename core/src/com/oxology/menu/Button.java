65dab423f133169e58d78679e71814caf847d9cd
OxologyGames <oxology.games@gmail.com>
2023-09-17 17:02:11 +0200

    Updated button

--- core/src/com/oxology/menu/Button.java
+++ core/src/com/oxology/menu/Button.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.oxology.screen.Template;
 
 public class Button {
     private String text;
@@ -12,40 +13,51 @@ public class Button {
     private float width, heigth;
     private BitmapFont font;
     private Action click;
-    private Action hover;
+    private boolean hover;
     private Texture button, buttonClick;
+
+    private Template screenTemplate;
 
     public interface Action {
         void onAction();
     };
 
-    public Button(float x, float y, String text, BitmapFont font, Action click, Action hover) {
+    public Button(float x, float y, String text, BitmapFont font, Action click, Template screenTemplate) {
         this.x = x;
         this.y = y;
         this.text = text;
         this.font = font;
         this.click = click;
-        this.hover = hover;
+        this.hover = false;
         this.button = new Texture("button.png");
         this.buttonClick = new Texture("buttonClick.png");
 
         GlyphLayout layout = new GlyphLayout(font, text);
         this.width = layout.width;
         this.heigth = layout.height;
+
+        this.screenTemplate = screenTemplate;
     }
 
     public void draw(SpriteBatch batch) {
-        batch.draw(button, x, y);
+
+        if(hover) {
+            batch.draw(buttonClick, x, y);
+        } else {
+            batch.draw(button, x, y);
+        }
         font.draw(batch, text, x+25-width/2, y-1);
     }
 
     public void update() {
-        if((Gdx.input.getX() >= x && Gdx.input.getX() < x + width) && (Gdx.input.getY() >= x && Gdx.input.getY() < y + heigth)) {
-            hover.onAction();
+        if((screenTemplate.getX() >= x && screenTemplate.getX() < x + button.getWidth()) && (screenTemplate.getY() >= y && screenTemplate.getY() < y + button.getHeight())) {
+            hover = true;
 
             if(Gdx.input.isTouched()) {
                 click.onAction();
             }
+        } else {
+            hover = false;
         }
     }
 }
