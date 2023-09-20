86783f5574548246de6cc087df002d3f2a245c96
Masiu121 <maks.kapolka@gmail.com>
2023-09-20 09:23:34 +0200

    Added level selector menu

---
+++ core/src/com/oxology/screen/menu/LevelSelector.java
@@ -0,0 +1,10 @@
+package com.oxology.screen.menu;
+
+import com.oxology.Runagate;
+import com.oxology.screen.Template;
+
+public class LevelSelector extends Template {
+    public LevelSelector(Runagate game) {
+        super(game);
+    }
+}
