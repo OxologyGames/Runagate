9a25c1699d944f1e03c79e088de39e5b77330b2a
Masiu121 <maks.kapolka@gmail.com>
2023-09-20 09:23:23 +0200

    Updated main menu

--- core/src/com/oxology/screen/menu/Main.java
+++ core/src/com/oxology/screen/menu/Main.java
@@ -1,6 +1,7 @@
 package com.oxology.screen.menu;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -9,19 +10,40 @@ import com.oxology.menu.Button;
 import com.oxology.screen.Template;
 
 public class Main extends Template {
-    private Button playButton;
+    private Button playBtn;
+    private Button worldBtn;
+    private Button exitBtn;
+
     private SpriteBatch batch;
+    private OrthographicCamera menuCamera;
 
     public Main(Runagate game) {
         super(game);
-        BitmapFont font = new BitmapFont(Gdx.files.internal("font/bahnschrift.fnt"));
+
+        menuCamera = new OrthographicCamera(384, 216);
+        menuCamera.translate(384/2f, 216/2f);
+        menuCamera.update();
+
+        BitmapFont font = new BitmapFont(Gdx.files.internal("font/PressStart2P.fnt"));
         font.setColor(1, 1, 1, 1);
-        font.getData().scaleX = .3f;
-        font.getData().scaleY = .3f;
-        playButton = new Button(100, 300, "Play", font, new Button.Action() {
+        font.getData().scaleX = .09f;
+        font.getData().scaleY = .09f;
+        playBtn = new Button(167, 150, "Play", font, new Button.Action() {
             @Override
             public void onAction() {
-                //System.out.println("clicked");
+                goToLevelSelector();
+            }
+        }, this);
+        worldBtn = new Button(167, 138, "Edit", font, new Button.Action() {
+            @Override
+            public void onAction() {
+                goToLevelEditor();
+            }
+        }, this);
+        exitBtn = new Button(167, 126, "Exit", font, new Button.Action() {
+            @Override
+            public void onAction() {
+                Gdx.app.exit();
             }
         }, this);
 
@@ -30,13 +52,42 @@ public class Main extends Template {
 
     @Override
     public void render(float deltaTime) {
-        camera.update();
-        batch.setProjectionMatrix(camera.combined);
-        ScreenUtils.clear(0, 1, 0, 1);
-        batch.begin();
-        playButton.draw(batch);
-        batch.end();
+        update(deltaTime);
+        ScreenUtils.clear(0, 0, 0, 1);
 
-        playButton.update();
+        batch.begin();
+        playBtn.draw(batch);
+        worldBtn.draw(batch);
+        exitBtn.draw(batch);
+        batch.end();
+    }
+
+    private void update(float deltaTime) {
+        menuCamera.update();
+        batch.setProjectionMatrix(menuCamera.combined);
+
+        playBtn.update();
+        worldBtn.update();
+        exitBtn.update();
+    }
+
+    public void goToLevelSelector() {
+        game.setScreen(new LevelSelector(game));
+    }
+
+    public void goToLevelEditor() {
+        game.setScreen(new WorldEditor(game));
+    }
+
+    @Override
+    public int getX() {
+        float prop = (float) Gdx.graphics.getWidth() / 384;
+        return (int) (Gdx.input.getX() / prop);
+    }
+
+    @Override
+    public int getY() {
+        float prop = (float) Gdx.graphics.getHeight() / 216;
+        return (int) ((Gdx.graphics.getHeight()-Gdx.input.getY()) / prop);
     }
 }
