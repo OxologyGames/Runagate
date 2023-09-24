5133b789e78f2c4c0ff5d3e6e98b6f54a1d8610a
Masiu121 <maks.kapolka@gmail.com>
2023-09-24 22:59:23 +0200

    Updated game screen for level loading

--- core/src/com/oxology/screen/Game.java
+++ core/src/com/oxology/screen/Game.java
@@ -4,14 +4,22 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.oxology.Runagate;
+import com.oxology.world.Level;
+import com.oxology.world.Tile;
 import com.oxology.world.entity.Player;
+
+import java.util.List;
 
 public class Game extends Template {
     private OrthographicCamera levelCamera;
     private SpriteBatch levelBatch;
-    private Player player;
+    private List<Level> levels;
 
-    public Game(Runagate game) {
+
+    //temp
+    private Texture wall;
+
+    public Game(Runagate game, List<Level> levels) {
         super(game);
 
         levelCamera = new OrthographicCamera(320, 180);
@@ -19,18 +27,38 @@ public class Game extends Template {
         levelCamera.update();
 
         levelBatch = new SpriteBatch();
+
+        this.levels = levels;
+        if(levels.get(0).getPlayer() == null)
+            levels.get(0).setPlayer(new Player(4, 6));
+
+        wall = new Texture("level/wall.png");
     }
 
     @Override
     public void render(float deltaTime) {
-        levelCamera.update();
-        levelBatch.setProjectionMatrix(levelCamera.combined);
+        update(deltaTime);
 
         levelBatch.begin();
-        levelBatch.draw(new Texture("level.png"), 0, 0);
+        for(int i = 0; i < 40; i++) {
+            for(int j = 0; j < 30; j++) {
+                if(levels.get(0).getTiles()[i][j] == Tile.WALL) {
+                    levelBatch.draw(wall, i*8, j*6);
+                }
+            }
+        }
+        levels.get(0).getPlayer().draw(levelBatch);
+        //levelBatch.draw(new Texture("level.png"), 0, 0);
         levelBatch.end();
 
         batch.begin();
         batch.end();
+    }
+
+    public void update(float deltaTime) {
+        levelCamera.update();
+        levelBatch.setProjectionMatrix(levelCamera.combined);
+
+        levels.get(0).update(deltaTime);
     }
 }
